f0d6b7754944727e332ef9ed7a2c6c53d58da33c
Lukas Nowaczek <acedude@wp.pl>
2016-02-10 19:07:57 +0100

    Added link for translations forums

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,6 +33,7 @@
                 <li><a href="{{ url('/pages/instructions') }}">Instructions</a></li>
                 <li><a href="{{ url('/pages/guidelines') }}">Guidelines</a></li>
                 <li><a href="{{ url('/pages/faq') }}">FAQ</a></li>
+                <li><a href="http://forums.unknownworlds.com/categories/subnautica-translations">Translations forums</a></li>
                 @if ( !Auth::guest() && Auth::user()->hasRole('Root'))
                     <li><a href="{{ url('/users') }}">Users</a></li>
                     <li><a href="{{ url('/roles') }}">Roles</a></li>
