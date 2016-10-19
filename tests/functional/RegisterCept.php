4202d9d380858244898255471049a6ae6ca29e44
Lukas Nowaczek <lukas@zirio.com>
2016-10-19 23:34:42 +0000

    Revert "Revert "Expanding tests""
    This reverts commit d854ac76ef1b0b43fcb11f4c0ea678d308252a76

---
+++ tests/functional/RegisterCept.php
@@ -0,0 +1,14 @@
+<?php 
+$I = new FunctionalTester($scenario);
+$I->wantTo('register a new account');
+
+$I->amOnPage('/');
+$I->click('Register');
+$I->fillField('name', 'Tester');
+$I->fillField('email', 'acedude@wp.pl');
+$I->fillField('password', 'kopytko5566');
+$I->fillField('password_confirmation', 'kopytko5566');
+$I->click('Register', 'form');
+
+$I->see('Tester');
+$I->seeRecord('App\User', ['name' => 'Tester']);
