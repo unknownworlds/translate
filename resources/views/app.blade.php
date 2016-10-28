b06b3c4e71d07c4594ee6284be377cc98149e9f3
Lukas Nowaczek <acedude@wp.pl>
2016-10-28 14:28:48 +0200

    Switched to production version of vuejs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -91,10 +91,8 @@
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-{{--<script src="//cdnjs.cloudflare.com/ajax/libs/vue/2.0.3/vue.js"></script>--}}
-{{--<script src="//cdnjs.cloudflare.com/ajax/libs/vue-resource/1.0.3/vue-resource.js"></script>--}}
-<script src="//vuejs.org/js/vue.js"></script>
-{{--<script src="//cdnjs.cloudflare.com/ajax/libs/vue-resource/1.0.3/vue-resource.min.js"></script>--}}
+<script src="//cdnjs.cloudflare.com/ajax/libs/vue/2.0.3/vue.js"></script>
+{{--<script src="//vuejs.org/js/vue.js"></script>--}}
 <script src="{{ asset('/js/translate.js') }}"></script>
 </body>
 </html>
