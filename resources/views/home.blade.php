30929c2702f935571d94fd8edf1c0a07d905c44b
Lukas Nowaczek <acedude@wp.pl>
2016-11-02 14:16:42 +0100

    Progress bar partial fix (lost a feature)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -116,12 +116,18 @@
                                                 </td>
                                                 <td>
                                                     <div class="progress">
-                                                        <div ng-class="{'progress-bar': true, 'progress-bar-info': {{$entry->count / $baseStringCounts[$entry->project_id]}} >= 0.8 && {{$entry->count / $baseStringCounts[$entry->project_id]}} < 1, 'progress-bar-success': {{$entry->count / $baseStringCounts[$entry->project_id]}} >= 1}"
+                                                        <div class="progress-bar"
                                                              role="progressbar" aria-valuenow="60"
                                                              aria-valuemin="0"
                                                              aria-valuemax="100"
                                                              style="width: {{ round($entry->count / $baseStringCounts[$entry->project_id] * 100, 3) }}%;">
                                                         </div>
+                                                        {{--<div ng-class="{'progress-bar': true, 'progress-bar-info': {{$entry->count / $baseStringCounts[$entry->project_id]}} >= 0.8 && {{$entry->count / $baseStringCounts[$entry->project_id]}} < 1, 'progress-bar-success': {{$entry->count / $baseStringCounts[$entry->project_id]}} >= 1}"--}}
+                                                             {{--role="progressbar" aria-valuenow="60"--}}
+                                                             {{--aria-valuemin="0"--}}
+                                                             {{--aria-valuemax="100"--}}
+                                                             {{--style="width: {{ round($entry->count / $baseStringCounts[$entry->project_id] * 100, 3) }}%;">--}}
+                                                        {{--</div>--}}
                                                     </div>
                                                 </td>
                                             </tr>
