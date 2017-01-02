cb4d5df737d529ad3897e9c030ff04202652ab88
Lukas Nowaczek <acedude@wp.pl>
2017-01-02 20:28:56 +0100

    App name set

--- config/app.php
+++ config/app.php
@@ -2,6 +2,18 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    */
+
+    'name' => 'translate.unknownworlds.com',
+
 	/*
 	|--------------------------------------------------------------------------
 	| Application Environment
--- resources/views/auth/emails/password.blade.php
+++
@@ -1,1 +0,0 @@
-Click here to reset your password: <a href="{{ url('password/reset/'.$token) }}">{{ url('password/reset/'.$token) }}</a>
