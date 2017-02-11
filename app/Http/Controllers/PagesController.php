288e172ded3d9142fd841f7e0692a07cbc940bd4
Lukas Nowaczek <acedude@wp.pl>
2017-02-11 01:29:03 +0100

    Return 404 for non-existent pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,20 +1,26 @@
 <?php
 /**
- * Copyright (c) Unknown Worlds Entertainment, 2016. 
+ * Copyright (c) Unknown Worlds Entertainment, 2016.
  * Created by Lukas Nowaczek <lukas@unknownworlds.com> <@lnowaczek>
  * Visit http://unknownworlds.com/
- * This file is a part of proprietary software. 
+ * This file is a part of proprietary software.
  */
 
 namespace App\Http\Controllers;
 
-class PagesController extends BaseApiController {
+class PagesController extends BaseApiController
+{
 
-	public function __construct() {
-	}
+    public function __construct()
+    {
+    }
 
-	public function index( $name ) {
-		return view( 'pages/' . $name );
-	}
+    public function index($name)
+    {
+        if (!file_exists(resource_path('views/pages/' . $name . '.blade.php')))
+            abort(404);
+
+        return view('pages/' . $name);
+    }
 
 }
