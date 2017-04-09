1749e9570447047b86007e9f2a3e6a523c4d7735
Lukas Nowaczek <lukas@zirio.com>
2017-04-09 23:02:59 +0200

    Search box will start processing the query after 500ms for better performance Removed unnecessary debug from js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -104,6 +104,7 @@
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="//cdn.jsdelivr.net/lodash/4.13.1/lodash.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/vue/2.0.3/vue.js"></script>
 {{--<script src="//vuejs.org/js/vue.js"></script>--}}
 <script src="{{ asset('/js/translate.js') }}"></script>
