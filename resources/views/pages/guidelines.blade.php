bba8821c661f0996560749aefbf629c3f6147dcb
Lukas Nowaczek <lukas@zirio.com>
2017-11-29 01:41:09 +0100

    Guidelines update for Subnautica

--- resources/views/pages/guidelines.blade.php
+++ resources/views/pages/guidelines.blade.php
@@ -15,7 +15,20 @@
                         <ul>
                             <li>Feel free to translate creature names</li>
                             <li>Translation must be the same case as the original</li>
-                            <li>Remember to include all special characters (like "&bull;") and placeholders (like "{0}")</li>
+                            <li>
+                                Remember to include all special characters (like "&bull;") and placeholders (like "{0}")
+                            </li>
+                            <li>Please leave empty strings as-is if you don't know what do do with them</li>
+                            <li>
+                                Translation keys similar to <b>Spinefish.TooltipIngredient</b> are alternative versions
+                                of given string ("Spinefish" in this example). They allow you to give the same string
+                                different formatting. Is's useful when the same string is used in multiple places.
+                            </li>
+                            <li>
+                                Language named "English (community)" allow you to propose your own, alternative version
+                                of an original, English text. You can use it when you find a typo or if you'd like to
+                                improve something else.
+                            </li>
                         </ul>
 
                     </div>
