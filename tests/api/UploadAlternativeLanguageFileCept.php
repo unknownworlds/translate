c190d5366468f99e3764b189710afc7d356a1601
Lukas Nowaczek <lukas@zirio.com>
2017-12-19 03:03:56 +0100

    Framework upgrade to Laravel 5.4, more tests

---
+++ tests/api/UploadAlternativeLanguageFileCept.php
@@ -0,0 +1,16 @@
+<?php
+$I = new ApiTester($scenario);
+$I->wantTo('test upload a translation file with modifications');
+
+$I->seeRecord('App\Project', ['name' => 'API test project']);
+
+$I->sendPOST('strings/translation-file', [
+	'api_key' => 'test_api_key_wow',
+	'data' => file_get_contents('tests/_data/English.json')
+]);
+$I->sendPOST('strings/translation-file', [
+	'api_key' => 'test_api_key_wow',
+	'data' => file_get_contents('tests/_data/English_alternative.json')
+]);
+
+$I->see('"Success!"');
