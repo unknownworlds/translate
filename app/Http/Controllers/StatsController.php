cacb68ce4940dd887590e684ae20ac7ebc1984b7
Lukas Nowaczek <lukas@zirio.com>
2018-09-28 13:31:54 +0200

    Limit stats on charts to last 6 months

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -4,31 +4,36 @@ namespace App\Http\Controllers;
 
 use App\TranslatedString;
 use App\User;
+use Carbon\Carbon;
 
-class StatsController extends Controller
-{
-    public function __construct()
-    {
-        $this->middleware(['auth', 'hasRole:Root']);
-    }
+class StatsController extends Controller {
+	public function __construct() {
+		$this->middleware( [ 'auth', 'hasRole:Root' ] );
+	}
 
-	public function translationsPerDay(){
+	public function translationsPerDay() {
 		// created
-		$createdData = TranslatedString::selectRaw('COUNT(*) AS count, DATE(created_at) AS date')->groupBy('date')->get();
-		$created = $createdData->pluck('count', 'date');
+		$createdData = TranslatedString::selectRaw( 'COUNT(*) AS count, DATE(created_at) AS date' )
+		                               ->where( 'created_at', '>=', Carbon::now()->subMonths( 6 ) )
+		                               ->groupBy( 'date' )
+		                               ->get();
+		$created     = $createdData->pluck( 'count', 'date' );
 
 		// voted up?
 		// voted down?
 		// accepted?
 		// deleted?
 
-		return view('stats.translations', compact('created'));
+		return view( 'stats.translations', compact( 'created' ) );
 	}
 
-	public function usersPerDay(){
-		$createdData = User::selectRaw('COUNT(*) AS count, DATE(created_at) AS date')->groupBy('date')->get();
-		$created = $createdData->pluck('count', 'date');
+	public function usersPerDay() {
+		$createdData = User::selectRaw( 'COUNT(*) AS count, DATE(created_at) AS date' )
+		                   ->where( 'created_at', '>=', Carbon::now()->subMonths( 6 ) )
+		                   ->groupBy( 'date' )
+		                   ->get();
+		$created     = $createdData->pluck( 'count', 'date' );
 
-		return view('stats.users', compact('created'));
+		return view( 'stats.users', compact( 'created' ) );
 	}
 }
