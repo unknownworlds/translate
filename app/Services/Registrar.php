a842de08ef6df18ef067335289136f1be294e2fe
Lukas Nowaczek <lukas@zirio.com>
2021-04-02 14:48:21 +0200

    Upgrade to Laravel 5.8

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -18,7 +18,7 @@ class Registrar implements RegistrarContract
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
+            'password' => 'required|confirmed|min:8',
         ]);
     }
 
