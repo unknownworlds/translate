272439ef791d86905b5d2e0838e0afb65d99ea9a
Lukas Nowaczek <lukas@zirio.com>
2021-04-03 13:48:42 +0200

    Upgrade bugfixes

--- app/Http/Controllers/AdminWhiteboardsController.php
+++ app/Http/Controllers/AdminWhiteboardsController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\AdminWhiteboard;
 use Auth;
-use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Request;
 
 class AdminWhiteboardsController extends BaseApiController
 {
